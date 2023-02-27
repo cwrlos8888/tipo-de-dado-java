9f9049a0215d3c2c5ab439d8927919de54ba594e
cwrlos8888 <bolanatrave1410@gmail.com>
2023-02-27 13:41:59 +0000

    salvar

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,4 +1,16 @@
 public class EstruturaDeRepeticao{
+    
+    public static void main(String[] args){
+
+        int cc = 0;
+        while (cc<4){
+            System.out.println("teste" + (cc+1));
+            cc++;
+        }
+
+
+
+    }
 
 
     
--- EstruturaDeSelecao.java
+++ EstruturaDeSelecao.java
@@ -1,4 +1,21 @@
 public class EstruturaDeSelecao{
+    public static void main(String[] args){
+
+        int idade;
+
+        idade=17;
+
+        if (idade >= 18)
+        {
+            System.out.print("Apto ao Serviço Militar Obrigatório!");
+
+            
+        } else {
+            System.out.print("Não esta apto ao Serviço Militar Obrigatório");
+        }
+
+
+    }
 
 
 
